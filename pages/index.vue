05390e9b72190860fded3f719115d3cf62cc0074
Szymon Wójcik <wojcik.szymon1@gmail.com>
2024-08-06 11:12:17 +0200

    style: home circles

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@ import DesktopHeader from "~/components/home/DesktopHeader.vue"
 import HomeDescription from "~/components/home/HomeDescription.vue"
 import HomeButtons from "~/components/home/HomeButtons.vue"
 import { computed } from "vue"
+import DottedCircle from "~/components/layout/DottedCircle.vue"
 
 const config = useRuntimeConfig()
 const { data } = await useFetch(`${config.public.api_url}api/race/playerCard`)
@@ -15,8 +16,8 @@ const getFirstPlayer = computed(() => {
 </script>
 
 <template>
-   <div class="flex flex-col justify-between bg-primary overflow-auto">
-      <MobileHeader class="lg:hidden" />
+   <div class="relative flex flex-col justify-between bg-primary overflow-auto">
+      <MobileHeader class="lg:hidden z-50" />
 
       <div class="flex flex-col lg:flex-row flex-1 items-start lg:items-center">
          <div
@@ -33,7 +34,23 @@ const getFirstPlayer = computed(() => {
          <HomeCards :best-card="getFirstPlayer" class="max-w-[800px]" />
       </div>
 
-      <HomePartners />
+      <HomePartners class="z-50" />
+
+      <DottedCircle
+         class="hidden lg:block"
+         right="5%"
+         top="5%"
+         size="150px"
+         color="rgba(141, 141, 141, 0.25)"
+      />
+
+      <DottedCircle
+         class="hidden lg:block"
+         right="25%"
+         bottom="5%"
+         size="150px"
+         color="rgba(141, 141, 141, 0.25)"
+      />
    </div>
 </template>
 
