9363c6cf49348352ec80833ee10407a81aea8550
Szymon Wójcik <wojcik.szymon1@gmail.com>
2024-09-22 08:29:09 +0200

    feat: server proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   devtools: { enabled: true },
+   devtools: { enabled: false },
+   routeRules: {
+      '/srv/**': {
+         proxy: { to: "http://localhost:3000/**", },
+         // proxy: { to: "https://api.wojolracing.pl/**", },
+      }
+   },
    colorMode: {
       preference: "dark",
    },
@@ -13,31 +19,25 @@ export default defineNuxtConfig({
       pageTransition: { name: "page", mode: "out-in" },
    },
    runtimeConfig: {
+      PORT: 26097,
+      NITRO_PORT: 26097,
       public: {
-         api_url: "http://localhost:3000/",
+         api_url: "/srv/",
          EMAIL_USER_ID: process.env.EMAIL_USER_ID,
          EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
          EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
       },
    },
    compatibilityDate: "2024-07-03",
-   modules: [
-      [
-         "@nuxtjs/google-fonts",
-         {
-            families: {
-               "Chakra Petch": true,
-               "M PLUS 1": "100..700",
-            },
+   modules: [[
+      "@nuxtjs/google-fonts",
+      {
+         families: {
+            "Chakra Petch": true,
+            "M PLUS 1": "100..700",
          },
-      ],
-      "@nuxt/eslint",
-      "vue3-carousel-nuxt",
-      '@samk-dev/nuxt-vcalendar',
-      "nuxt-facebook-chat",
-      "@nuxt/ui",
-      "@vueuse/nuxt",
-   ],
+      },
+   ], "@nuxt/eslint", "vue3-carousel-nuxt", '@samk-dev/nuxt-vcalendar', "nuxt-facebook-chat", "@nuxt/ui", "@vueuse/nuxt"],
    css: ["~/assets/css/main.css"],
    postcss: {
       plugins: {
